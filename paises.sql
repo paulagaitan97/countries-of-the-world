ace20f38015efe5f3caff93a9e251e769f61ccfd
Paula Andrea Gaitán <paulagaiitan@gmail.com>
2020-08-19 08:48:12 -0500

    Add files via upload

---
+++ paises.sql
@@ -0,0 +1,7 @@
+En una rifa o loteria solo se puede pagar con un tipo de moneda? (La moneda que establece el administrador al crearla)
+
+
+valor minimo que se podria adquirir en criptomonedas.?
+
+
+el premio de una rifa corresponde al mismo tipo de criptomoneda.?
